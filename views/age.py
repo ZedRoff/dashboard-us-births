c6704ddb6867594e14cfc1f4a3a548c1a1e87a5f
ghazanfa <aman.ghazanfar@edu.esiee.fr>
2025-01-16 11:19:38 +0100

    feat: map and tendancy graph

--- views/age.py
+++ views/age.py
@@ -1,9 +1,9 @@
 from dash import html, dcc
 import components.ageGlob as ageGlob
-import components.motherAge as motherAge
+import components.motherAgeGlob as motherAgeGlob
 
 def show():
     return html.Div([
         dcc.Graph(id="test", figure=ageGlob.generate()),
-        dcc.Graph(id="test3", figure=motherAge.generate())
+        dcc.Graph(id="test3", figure=motherAgeGlob.generate())
     ])
--- views/births.py
+++ views/births.py
@@ -1,6 +1,8 @@
 from dash import html, dcc
 import components.US_map as US_map
+import components.tendanceGlob as tendanceGlob
 def show():
     return html.Div([
-        dcc.Graph(id="test", figure=US_map.create_plotly_map(2020))
+        dcc.Graph(id="test", figure=US_map.create_plotly_map(2020)),
+        dcc.Graph(id="test1", figure=tendanceGlob.generate())
     ])
